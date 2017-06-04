ab0e43d0b3c6345df6999ac70bdc979cb5123e93
Rovashan <rovashan.subbrayan@sgits.co.za>
2017-06-04 10:21:06 +0200

    WiP

--- app/src/main/java/com/cad/halstart/MainActivity.java
+++ app/src/main/java/com/cad/halstart/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.FileObserver;
 import android.os.Handler;
 import android.os.Message;
 import android.text.TextUtils;
@@ -15,6 +16,11 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStreamWriter;
 
 public class MainActivity extends Activity {
 
@@ -26,11 +32,17 @@ public class MainActivity extends Activity {
     public MyFileObserver fileWatcher = new MyFileObserver("/sdcard/ScanPrintApi/") {
         @Override
         public void onEvent(int event, String path) {
-            Log.i("FileObserver.CREATE", "Rover");
+            if ((FileObserver.CREATE & event)!=0) {
+                Log.i("FileObserver.CREATE", "Rover");
 
-            if (path.equals("Scan.txt")) {
-                Scan();
-                DeleteScanFile();
+                if (path.equals("Scan.txt")) {
+                    Scan();
+                    DeleteScanFile();
+                }
+
+                if (path.equals("pls_print.txt")) {
+                    PrintFile();
+                }
             }
         }
     };
@@ -43,6 +55,7 @@ public class MainActivity extends Activity {
             if (!TextUtils.isEmpty(msg.obj.toString())) {
                 String str = msg.obj.toString();
                 MainActivity.this.txtBarcode.setText(str);
+                CreateScanResultFile(str);
             }
         }
     }
@@ -121,9 +134,36 @@ public class MainActivity extends Activity {
         }
     }
 
-
     public void onClickDeleteScanFile(View v) {
         DeleteScanFile();
+    }
+
+    public void CreateScanResultFile(String scanStr) {
+        if (!Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)){
+            Log.e("SDCARD ", "No SD Card present");
+        } else {
+            try {
+                File newFolder = new File(Environment.getExternalStorageDirectory(), "ScanPrintApi");
+                if (!newFolder.exists()) {
+                    newFolder.mkdir();
+                }
+                try {
+                    File file = new File(newFolder, "ScanResult" + ".txt");
+                    file.createNewFile();
+
+                    FileOutputStream fOut = new FileOutputStream(file);
+                    OutputStreamWriter myOutWriter = new OutputStreamWriter(fOut);
+                    myOutWriter.append(scanStr);
+
+                    myOutWriter.close();
+
+                } catch (Exception ex) {
+                    System.out.println("ex: " + ex);
+                }
+            } catch (Exception e) {
+                System.out.println("e: " + e);
+            }
+        }
     }
 
     public void DeleteScanFile() {
@@ -147,4 +187,59 @@ public class MainActivity extends Activity {
         }
     }
 
+    public void PrintFile() {
+        if (!Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)){
+            Log.e("SDCARD ", "No SD Card present");
+        } else {
+            try {
+                File newFolder = new File(Environment.getExternalStorageDirectory(), "ScanPrintApi");
+                if (!newFolder.exists()) {
+                    newFolder.mkdir();
+                }
+                try {
+                    File file = new File(newFolder, "pls_print" + ".txt");
+                    byte[] printContent = ReadBytesFromFile(file);
+                    this.mPrintConnect.send(printContent);
+                } catch (Exception ex) {
+                    System.out.println("ex: " + ex);
+                }
+            } catch (Exception e) {
+                System.out.println("e: " + e);
+            }
+        }
+    }
+
+    public static byte[] ReadBytesFromFile(File file) throws IOException {
+        InputStream is = new FileInputStream(file);
+
+        // Get the size of the file
+        long length = file.length();
+
+        // You cannot create an array using a long type.
+        // It needs to be an int type.
+        // Before converting to an int type, check
+        // to ensure that file is not larger than Integer.MAX_VALUE.
+        if (length > Integer.MAX_VALUE) {
+            throw new IOException("Could not completely read file " + file.getName() + " as it is too long (" + length + " bytes, max supported " + Integer.MAX_VALUE + ")");
+        }
+
+        // Create the byte array to hold the data
+        byte[] bytes = new byte[(int)length];
+
+        // Read in the bytes
+        int offset = 0;
+        int numRead = 0;
+        while (offset < bytes.length && (numRead=is.read(bytes, offset, bytes.length-offset)) >= 0) {
+            offset += numRead;
+        }
+
+        // Ensure all the bytes have been read in
+        if (offset < bytes.length) {
+            throw new IOException("Could not completely read file " + file.getName());
+        }
+
+        // Close the input stream and return bytes
+        is.close();
+        return bytes;
+    }
 }
